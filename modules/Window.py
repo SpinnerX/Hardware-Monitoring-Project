9abb0c446aab71b56f6c5bcb5469cd2cbf67f311
= <=>
2023-12-09 20:00:45 -0800

    Working Window class for right now to allow everyone have a template starter code, for testing each of your preliminary features

--- modules/Window.py
+++ modules/Window.py
@@ -1,5 +1,4 @@
 import pygame
-from interfaces.widgets.Button import *
 
 class Window:
     def __init__(self, title: str, w: int, h: int) -> None:
@@ -14,8 +13,6 @@ class Window:
         self.screen.blit(self.surface, (0, 0))
         self.FPS = 60
         self.timestep = pygame.time.Clock()
-        cyanColor = pygame.Color(224, 255, 255) # Light cyan
-        self.testButton = Button(cyanColor, x=1050, y=150, width=130, height=50, text="New Maze")
 
     # Setting the color
     def setColor(self, color):
@@ -26,13 +23,11 @@ class Window:
 
     # Running the mainloop
     def mainloop(self):
-
         while self.isRunning:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     self.isRunning = False
             
-            self.testButton.draw(self.screen)
             self.update()
             self.timestep.tick(self.FPS)
 
