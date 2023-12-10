32a758508a3fead3f63c377517ddad703f049042
InformalFormalities <MyCollegeBoardAndSchoolEmail@gmail.com>
2023-12-10 01:17:31 -0800

    Working on parsing

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
+import os
 import platform
 import psutil
-import cpuinfo
 import GPUtil
+import cpuinfo
+from tabulate import tabulate
 
 def main():
+	LINES_USEFUL_INFORMATION = 11;
+
+	for file in os.listdir(os.getcwd()):
+		for row in csvfile:
+			list = []
+			for i in range(len(LINES_USEFUL_INFORMATION)):
+				list.append(
+
+"""
 	#Documentation: https://docs.python.org/3/library/platform.html
 	print(f"Machine: {platform.machine()}")
 	print(f"Architecture: {platform.architecture()}")
@@ -18,12 +29,17 @@ def main():
 	print(f"UName: {platform.uname().processor}")
 
 	#Documentation: https://github.com/workhorsy/py-cpuinfo
-	print(f"CPU Info: {cpuinfo.get_cpu_info()}");
+	print(f"CPU Info: {cpuinfo.get_cpu_info()}")
+#	print(f"CPU Info: {cpuinfo.get_cpu_info()['brand_raw']}")
 
 	#Documentation: https://github.com/giampaolo/psutil/blob/master/README.rst
+	print(f"psutil Info: {psutil.cpu_times()}")
 	
-
 	#Documentation: https://github.com/anderskm/gputil
+	GPUs = GPUtil.getGPUs()
+	for x in GPUs:
+		print(f"GPU Name: {x.name}")
+"""
 	
 
 
